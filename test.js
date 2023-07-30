2a019fbc2ab6d58a66f031ead4664e9a6bd082e4
Andrew Santoyo <andrewsantoyo100@gmail.com>
2023-07-30 15:31:06 -0700

    Update CSS

--- test.js
+++ test.js
@@ -11,12 +11,15 @@ document.getElementById("add-button").addEventListener("click", handleClick);
 
 // Test code
 function test() {
-  const mockInputValue = 'Test Product';
+  const mockInputValue = 'Test Success';
   document.getElementById("input-field").value = mockInputValue;
 
   handleClick(); // Manually call the click event handler
 
   // Check the console for the logged message
+
+  // Reset the input field value to an empty string after the test is completed
+  document.getElementById("input-field").value = '';
 }
 
 test(); // Run the test
