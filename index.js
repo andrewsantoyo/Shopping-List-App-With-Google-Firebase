243ecd1402286d92a0ae32680be5d9e61ff9e413
Andrew Santoyo <andrewsantoyo100@gmail.com>
2023-07-29 23:37:43 -0700

    Initialize project structure

---
+++ index.js
@@ -0,0 +1,10 @@
+// When you click the 'Add to cart' button, whatever is written should be console logged.
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
+
+addButtonEl.addEventListener("click", function() {
+    // Will use inputValue later
+    let inputValue = inputFieldEl.value
+
+    console.log("Clicked the 'Add to cart' button and the input field is: ", inputValue)
+})
